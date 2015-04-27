0a6fac90e6d2cfa2abfae647be19cb120ffbbe65
Shoaib Ahmed <shoaib30@gmail.com>
2015-04-27 21:02:58 +0530

    updated lsitframe

--- list_frame.php
+++ list_frame.php
@@ -3,23 +3,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head><title>List Frames</title></head>
 
-<?php &id=$_GET["id"];?>
+<?php $id=$_GET["id"];?>
 
 <frameset cols="200pt,*" frameborder="0" border="0" framespacing="0"> 
 	<frame src= <?php
-	if(id==0)
+	if($id==0)
 		echo "movie_side_bar.xhtml";
-	elseif(id==1)
-		echo "movie_side_bar.xhtml";
-	elseif(id==1)
-		echo "movie_side_bar.xhtml"; ?>
+	elseif($id==1)
+		echo "actor_side_bar.xhtml";
+	elseif($id==2)
+		echo "movie_side_bar.xhtml"; ?>	
 		noresize="noresize" scrolling="no"/>
 	<frame src= <?php
-	if(id==0)
+	if($id==0)
 		echo "movie_list.xhtml";
-	elseif(id==1)
-		echo "movie_list.xhtml";
-	elseif(id==1)
+	elseif($id==1)
+		echo "actor_list.xhtml";
+	elseif($id==2)
 		echo "movie_list.xhtml"; ?> frameborder="0" noresize="noresize" name="list_frame"/>
 </frameset>
 </html>
