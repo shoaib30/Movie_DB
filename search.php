5fe382f93bcfe22c2a0fc41c43e6e13b105bf8da
Shoaib Ahmed <shoaib30@gmail.com>
2015-05-04 23:35:36 +0530

    search

---
+++ search.php
@@ -0,0 +1,45 @@
+<?php 
+	$dbhost="localhost";
+	$dbuser="sid";
+	$dbpass="Shoaib@123";
+	$dbname="movie_db";
+	$connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+	
+	if(mysqli_connect_error())
+	{
+		die("database failed". mysqli_connect_error(). " ". mysqli_connect_errno());
+	}
+	require_once("redirect.php");
+?>
+
+<?php
+	$cat=$_POST["category"];
+	$val=$_POST["search"];
+	echo "$cat,$val";
+	if($cat==1)
+		$query="select MNO from MOVIE where MNAME='$val';";
+	elseif($cat==2)
+		$query="select ANO from ACTOR where ANAME='$val';";
+	elseif($cat==3)
+		$query="select DNO from DIRECTORS where DNAME='$val';";
+	$result= mysqli_query($connection,$query);
+	if(!$result)
+		die("nothing");
+	$row=mysqli_fetch_assoc($result);
+	if($cat==1)
+	{
+		$red=$row["MNO"];
+		redirect_to("./Individual_pages/indimovie.php?mno=$red");
+	}
+	elseif($cat==2)
+	{
+		$red=$row["ANO"];
+		redirect_to("./Individual_pages/indiactor.php?ano=$red");
+	}
+	elseif($cat==3)
+	{
+		$red=$row["DNO"];
+		redirect_to("./Individual_pages/indidirector.php?dno=$red");
+	}
+	echo $red;
+?>
